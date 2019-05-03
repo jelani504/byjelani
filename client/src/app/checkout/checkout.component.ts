9344239763213d380d345de8a233190a52e20cc7
jelani <jhankins5@dxc.com>
2019-05-03 13:07:05 -0500

    (add) orderForm

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { NavigationService } from '../navigation.service';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -11,6 +12,17 @@ export class CheckoutComponent implements OnInit {
 
   public vmUserBag = [];
   public vmOrderTotal;
+  public orderForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    company: new FormControl('', Validators.required),
+    country: new FormControl('', Validators.required),
+    state: new FormControl('', Validators.required),
+    zip: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+    streetAddress: new FormControl('', Validators.required),
+  });
+
   constructor(
     private userService: UserService,
     public navigationService: NavigationService
@@ -26,6 +38,10 @@ export class CheckoutComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  payClick(){
+    console.log(this.orderForm);
   }
 
 }
