3c7f41a0ed5b21f0223efe6945795b16e8ddb704
jelani <jhankins5@dxc.com>
2019-05-03 13:38:33 -0500

    (add) header bag icon number

--- client/src/app/head/head.component.ts
+++ client/src/app/head/head.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IconService } from '../icon.service';
 import { NavigationService } from '../navigation.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-head',
@@ -10,10 +11,14 @@ import { NavigationService } from '../navigation.service';
 export class HeadComponent implements OnInit {
 
   public locationOrigin = window.location.origin;
+  public vmBagLength = 0;
   constructor(
     public iconService: IconService,
-    public navigationService: NavigationService
-  ) {}
+    public navigationService: NavigationService,
+    private userService: UserService
+  ) {
+    userService.userBag.subscribe(bag => this.vmBagLength = bag.length);
+  }
 
   ngOnInit() {
   }
