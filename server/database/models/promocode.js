bfd4b1b57fbe1b5d781ec15f01102c93f70c6788
jelani <jhankins5@dxc.com>
2019-05-10 11:12:19 -0500

    (create) promo code model

---
+++ server/database/models/promocode.js
@@ -0,0 +1,25 @@
+const { Schema } = mongoose;
+
+const promoCodeSchema = new Schema({
+  code: String,
+  discount: Number
+});
+
+const PromoCode = mongoose.model('PromoCode', promoCodeSchema);
+
+const promoCodeHelpers = {
+  createPromoCode: async (promoCode) => await new PromoCode(promoCode).save(),
+  findOnePromoCode: async (code) => await PromoCode.findOne({code}),
+  findAllPromoCodes: async () => await PromoCode.find({}),
+  updatePromoCode: async (code, key, value) => {
+    const promoCode = await PromoCode.findOne({code});
+    promoCode[key] = value;
+    // console.log(promoCode, 'Promo code');
+    return promoCode.save();
+  }
+};
+
+// const allPromoCodes = promoCodeHelpers.findAllPromoCodes();
+// console.log(allPromoCodes, 'ALL PROMO CODES ORDERS');
+PromoCode.remove({}, () => console.log('promo codes collection removed'));
+module.exports = { PaypalOrder, promoCodeHelpers };
